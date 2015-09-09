8dd43b17ca7b6097800e5798e354a9987cdc46fa
YuHuaiChen <tp6m4cj96@gmail.com>
2015-09-10 03:09:43 +0800

    paste things in...

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # meteor-darkroomjs
-Meteor wrapper for DarkroomJS: Extensible image editing tool (https://github.com/MattKetmo/darkroomjs)
+Meteor wrapper for DarkroomJS: Extensible image editing tool
+Original GitHub Repo: [DarkroomJS](https://github.com/MattKetmo/darkroomjs)
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'clipo:darkroomjs',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: 'Meteor wrapper for DarkroomJS: Extensible image editing tool (https://github.com/MattKetmo/darkroomjs)',
+  summary: 'Meteor wrapper for DarkroomJS: Extensible image editing tool',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/clipo-info/meteor-darkroomjs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.addFiles('darkroomjs.js');
+  api.use('ryanswapp:fabricjs');
+  api.addFiles('client/darkroomjs.min.js', ['client']);
+  api.addFiles('client/darkroomjs.min.css', ['client']);
 });
 
 Package.onTest(function(api) {
