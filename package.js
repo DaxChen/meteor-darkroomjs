82a12beaaa9554e9c711bc53fda43270596040d4
YuHuaiChen <tp6m4cj96@gmail.com>
2015-09-11 21:25:01 +0800

    bump and publish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clipo:darkroomjs',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Meteor wrapper for DarkroomJS: Extensible image editing tool',
   // URL to the Git repository containing the source code for this package.
